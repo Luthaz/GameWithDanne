084c4750e772c28110975fa5aa0feaaeb30fb21a
Dexter <kastrullkastaren@hotmail.coim>
2017-08-10 16:24:02 +0200

    Commit

---
+++ GameWithD/src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Lukas on 10/08/2017.
+ */
+public class Test {
+    public static void test(){
+        System.out.println("Testet funkar");
+    }
+}
---
+++ GameWithD/src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Lukas on 10/08/2017.
+ */
+public class main {
+    public static void main(String[] Args){
+        Test test = new Test();
+        test.test();
+    }
+}
